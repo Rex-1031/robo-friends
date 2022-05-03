36c3977c9c501d94f0dbacbc243c4d0c331d1fc0
Rex-1031 <steventclair@gmail.com>
2022-05-03 15:56:04 -0400

    adding redux to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import 'tachyons'
 import App from '../src/containers/App';
 import './index.css';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots)
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
